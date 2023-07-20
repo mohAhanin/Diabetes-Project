bef7bc0d846cc0af8a7ae5ff0356ede34995e5f4
taa <TahaM8000@gmail.com>
2023-07-20 23:00:03 +0330

    update panel admin for surveyInfo

--- django/survey/admin.py
+++ django/survey/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import *
+from .models import SurveyInfo
 
-admin.site.register(SurveyInfo)
+class SurveyInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Pregnancies', 'result', 'isTrain', 'isTest')
+    list_filter = ('isTrain', 'isTest')
+
+admin.site.register(SurveyInfo, SurveyInfoAdmin)
